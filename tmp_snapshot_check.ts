c643c12efcf32b5323be56239c8a7ea7abb2ee64
Pankoke <traden.pankoke@gmail.com>
2025-11-17 14:42:48 +0100

    Action02

---
+++ tmp_snapshot_check.ts
@@ -0,0 +1,6 @@
+import { berlinDateString } from './lib/timezone';
+import { loadSnapshotForLocale } from './lib/news/snapshot';
+(async () => {
+  console.log('Berlin today', berlinDateString(new Date()));
+  console.log(await loadSnapshotForLocale('de'));
+})();
